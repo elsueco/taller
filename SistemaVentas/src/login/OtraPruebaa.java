17f0ea8c17c6fbe747688069f96d2b4348d74307
Johel <Johel@Johel-PC>
2013-09-29 13:59:36 -0500

    aqui

--- SistemaVentas/src/login/OtraPruebaa.java
+++ SistemaVentas/src/login/OtraPruebaa.java
@@ -4,7 +4,7 @@ public class OtraPruebaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("que paso");
+System.out.println("que paso calichin");
 	}
 
 }
