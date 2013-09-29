1afedeb6b0e62f33ecf5a9b577606f9c26526f0f
Johel <Johel@Johel-PC>
2013-09-29 15:23:35 -0500

    cambio de clases

---
+++ SistemaVentas/src/com/satellite/model/OtraPruebaa.java
@@ -0,0 +1,10 @@
+package com.satellite.model;
+
+public class OtraPruebaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("que paso calichin");
+	}
+
+}
---
+++ SistemaVentas/src/com/satellite/model/main.java
@@ -0,0 +1,10 @@
+package com.satellite.model;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola mundo en git version 3");
+	}
+
+}
